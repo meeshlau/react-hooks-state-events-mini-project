88e2be5fb92b232a788ca13a7c69842b00bfa9cf
Michelle Lau <meeshklau@gmail.com>
2022-06-24 15:15:03 -1000

    done

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,16 @@
 import React from "react";
+import Task from "./Task"
 
-function TaskList() {
+function TaskList({ tasks }) {
+  const tasksArray = tasks.map((task, index) => {
+    return <Task 
+    key={index} 
+    text={task.text} 
+    category={task.category}/>
+  })
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {tasksArray}
     </div>
   );
 }
